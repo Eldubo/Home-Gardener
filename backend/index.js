8a7f60f46578e4e6c575cf1f6f18ad85a351d76d
Lola-Nieto <48588470@est.ort.edu.ar>
2025-06-30 12:14:27 -0300

    actualizando rutas en index

--- backend/index.js
+++ backend/index.js
@@ -2,15 +2,17 @@ import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 import cors from 'cors';
-import authRoutes from './controllers/auth-controller.js';
+
+import AuthRoutes from './controllers/auth-controller.js';
+import PlantasRoutes from './controllers/plantas-controller.js';
 
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/auth', authRoutes);
-app.use('/api/plantas', authRoutes);
+app.use('/api/auth', AuthRoutes);
+app.use('/api/plantas', PlantasRoutes);
 
 
 
