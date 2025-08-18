d1500e72e34605de54cf5186f0856205cce8d734
Android Studio <devandroid@ort>
2025-08-18 08:58:18 -0300

    agregar ambiente en el front

--- frontend/src/navigation/AppNavigator.js
+++ frontend/src/navigation/AppNavigator.js
@@ -24,7 +24,7 @@ export default function AppNavigator() {
   return (
     <Stack.Navigator initialRouteName="Splash" screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Splash" component={SplashScreen} />
-      <Stack.Screen name="Bienvenido" component={HealthCheck} />
+      <Stack.Screen name="Bienvenido" component={BienvenidoScreen} />
       <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="Register" component={RegisterScreen} />
       <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
